77c8b38b48d8cbfebb5e7ca5921b3ab8d71a3b39
Steve Mings <stevennmings@gmail.com>
2017-11-22 00:33:00 -0600

    Final Commit

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -33,7 +33,7 @@ namespace TechJobsConsole
                         jobsResult.Add(job);
                     }
                 }
-                
+
             }
             return jobsResult;
         }
